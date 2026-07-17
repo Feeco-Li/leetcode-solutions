d7103d02dcb0a7d7cc9174a22e7620a5c1fb38d1
Feeco <lifeike67@gmail.com>
2026-07-17 12:15:01 -0600

    86. Partition List
    compact version

---
+++ 86-partition-list/solution.py
@@ -0,0 +1,52 @@
+# 86: Partition List
+# Difficulty: Medium
+# https://leetcode.com/problems/partition-list/
+#
+# Given the `head` of a linked list and a value `x`, partition it such that all
+# nodes **less than** `x` come before nodes **greater than or equal** to `x`.
+# 
+# You should **preserve** the original relative order of the nodes in each of the
+# two partitions.
+# 
+#  
+# 
+# **Example 1:**
+# 
+# **Input:** head = [1,4,3,2,5,2], x = 3
+# **Output:** [1,2,2,4,3,5]
+# 
+# **Example 2:**
+# 
+# **Input:** head = [2,1], x = 2
+# **Output:** [1,2]
+# 
+#  
+# 
+# **Constraints:**
+# * The number of nodes in the list is in the range `[0, 200]`.
+# * `-100 <= Node.val <= 100`
+# * `-200 <= x <= 200`
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def partition(self, head: Optional[ListNode], x: int) -> Optional[ListNode]:
+        small, large = ListNode(-1), ListNode(-1)
+        pSmall, pLarge = small, large
+        
+        p = head
+        while p:  # Python 中 while p is not None 可以简写为 while p
+            if p.val < x:
+                pSmall.next = p
+                pSmall = p # 合并写法：pSmall.next, pSmall = p, p
+            else:
+                pLarge.next = p
+                pLarge = p
+            p = p.next
+            
+        pLarge.next = None
+        pSmall.next = large.next
+        return small.next
