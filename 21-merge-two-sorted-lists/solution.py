0a6e904fb605b8a04634e8e774cdd48cf173273c
Feeco <lifeike67@gmail.com>
2026-07-18 21:52:35 -0600

    21. Merge Two Sorted Lists
    review 1

--- 21-merge-two-sorted-lists/solution.py
+++ 21-merge-two-sorted-lists/solution.py
@@ -3,31 +3,31 @@
 # https://leetcode.com/problems/merge-two-sorted-lists/
 #
 # You are given the heads of two sorted linked lists `list1` and `list2`.
-# 
+#
 # Merge the two lists into one **sorted** list. The list should be made by
 # splicing together the nodes of the first two lists.
-# 
+#
 # Return *the head of the merged linked list*.
-# 
-#  
-# 
+#
+#
+#
 # **Example 1:**
-# 
+#
 # **Input:** list1 = [1,2,4], list2 = [1,3,4]
 # **Output:** [1,1,2,3,4,4]
-# 
+#
 # **Example 2:**
-# 
+#
 # **Input:** list1 = [], list2 = []
 # **Output:** []
-# 
+#
 # **Example 3:**
-# 
+#
 # **Input:** list1 = [], list2 = [0]
 # **Output:** [0]
-# 
-#  
-# 
+#
+#
+#
 # **Constraints:**
 # * The number of nodes in both lists is in the range `[0, 50]`.
 # * `-100 <= Node.val <= 100`
@@ -39,26 +39,29 @@
 #         self.val = val
 #         self.next = next
 class Solution:
-    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
-        dummy=ListNode(-1)
-        p=dummy
-        p1=list1
-        p2=list2
+    def mergeTwoLists(
+        self, list1: Optional[ListNode], list2: Optional[ListNode]
+    ) -> Optional[ListNode]:
+        pA = list1
+        pB = list2
+        dummy = ListNode(-1)
+        p = dummy
+        if not pA and not pB:
+            return None
 
-        while p1 is not None and p2 is not None:
-            if p1.val>p2.val:
-                p.next=p2
-                p2=p2.next
+        while pA and pB:
+            if pA.val <= pB.val:
+                p.next = pA
+                pA = pA.next
             else:
-                p.next=p1
-                p1=p1.next
-            p=p.next
+                p.next = pB
+                pB = pB.next
 
-        if p1 is not None:
-            p.next = p1
+            p = p.next
 
-        if p2 is not None:
-            p.next = p2
+        if pA is None:
+            p.next = pB
+        else:
+            p.next = pA
 
         return dummy.next
-
