fe41aa7e4b6b9b231d47abb635713d618ac4b68d
Feeco <lifeike67@gmail.com>
2026-07-20 03:22:39 -0600

    144. Binary Tree Preorder Traversal first version

--- 144-binary-tree-preorder-traversal/solution.py
+++ 144-binary-tree-preorder-traversal/solution.py
@@ -4,47 +4,47 @@
 #
 # Given the `root` of a binary tree, return *the preorder traversal of its nodes'
 # values*.
-# 
-#  
-# 
+#
+#
+#
 # **Example 1:**
-# 
+#
 # **Input:** root = [1,null,2,3]
-# 
+#
 # **Output:** [1,2,3]
-# 
+#
 # **Explanation:**
-# 
-# 
+#
+#
 # **Example 2:**
-# 
+#
 # **Input:** root = [1,2,3,4,5,null,8,null,null,6,7,9]
-# 
+#
 # **Output:** [1,2,4,5,6,7,3,8,9]
-# 
+#
 # **Explanation:**
-# 
-# 
+#
+#
 # **Example 3:**
-# 
+#
 # **Input:** root = []
-# 
+#
 # **Output:** []
-# 
+#
 # **Example 4:**
-# 
+#
 # **Input:** root = [1]
-# 
+#
 # **Output:** [1]
-# 
-#  
-# 
+#
+#
+#
 # **Constraints:**
 # * The number of nodes in the tree is in the range `[0, 100]`.
 # * `-100 <= Node.val <= 100`
-# 
-#  
-# 
+#
+#
+#
 # **Follow up:** Recursive solution is trivial, could you do it iteratively?
 
 # Definition for a binary tree node.
@@ -55,4 +55,13 @@
 #         self.right = right
 class Solution:
     def preorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
-        
+        self.q = []
+        self.dfs(root)
+        return self.q
+
+    def dfs(self, root: Optional[TreeNode]):
+        if root is None:
+            return
+        self.q.append(root.val)
+        self.dfs(root.left)
+        self.dfs(root.right)
