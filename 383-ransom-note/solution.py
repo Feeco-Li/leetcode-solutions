17cd33526db1a17cde038b480962890b0c728dae
Feeco <lifeike67@gmail.com>
2026-07-31 11:54:16 -0600

    383. Ransom Note first version

---
+++ 383-ransom-note/solution.py
@@ -0,0 +1,47 @@
+# 383: Ransom Note
+# Difficulty: Easy
+# https://leetcode.com/problems/ransom-note/
+#
+# Given two strings `ransomNote` and `magazine`, return `true`* if *`ransomNote`*
+# can be constructed by using the letters from *`magazine`* and *`false`*
+# otherwise*.
+#
+# Each letter in `magazine` can only be used once in `ransomNote`.
+#
+#
+#
+# **Example 1:**
+#
+# **Input:** ransomNote = "a", magazine = "b"
+# **Output:** false
+#
+# **Example 2:**
+#
+# **Input:** ransomNote = "aa", magazine = "ab"
+# **Output:** false
+#
+# **Example 3:**
+#
+# **Input:** ransomNote = "aa", magazine = "aab"
+# **Output:** true
+#
+#
+#
+# **Constraints:**
+# * `1 <= ransomNote.length, magazine.length <= 10⁵`
+# * `ransomNote` and `magazine` consist of lowercase English letters.
+from collections import Counter
+
+
+class Solution:
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        cR = Counter(ransomNote)
+        cM = Counter(magazine)
+        print(cR)
+        print(cM)
+        for key in cR.keys():
+            # print(cR[key])
+            # print(cM[key])
+            if cR[key] > cM[key]:
+                return False
+        return True
